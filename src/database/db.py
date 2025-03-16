4c73feae4fa0cd2a64432a7f92ba1a6fbe158dab
Anna Tsepilova <virgo.tsav@gmail.com>
2025-03-16 20:33:33 +0200

    Removed unused code

--- src/database/db.py
+++ src/database/db.py
@@ -34,33 +34,3 @@ sessionmanager = DatabaseSessionManager(config.DB_URL)
 async def get_db():
     async with sessionmanager.session() as session:
         yield session
-
-
-
-
-# from sqlalchemy import create_engine, Integer, String, Boolean
-# from sqlalchemy.orm import sessionmaker, DeclarativeBase, mapped_column, Mapped
-
-# DATABASE_URL = "postgresql://university:university@10.0.10.10/uni-hw6"
-# engine = create_engine(DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# class Base(DeclarativeBase):
-#     pass
-
-# class Note(Base):
-#     __tablename__ = "notes"
-#     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-#     name: Mapped[str] = mapped_column(String(50))
-#     description: Mapped[str] = mapped_column(String(250))
-#     done: Mapped[bool] = mapped_column(Boolean, default=False)
-
-# Base.metadata.create_all(bind=engine)
-
-# # Dependency
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
